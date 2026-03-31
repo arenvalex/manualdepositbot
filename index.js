c8007abbecb49b45e187fd6884588c0d0b0af412
arenvalex <myketyson609@gmail.com>
2026-03-31 18:57:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const fetch = require("node-fetch");
 
 const token = process.env.TOKEN;
-const SHEET_URL = "https://script.google.com/macros/s/AKfycbyHaSKcRznP6KsjUVmuSaYnQxlYzyk7FgBvqJxn43ImlmhK2JLhabubHNYcpVzVpS0c/exec";
+const SHEET_URL = "https://script.google.com/macros/s/AKfycbzcpUafU7zAewaz1_PitM8wOKacFsXA1yDkBc6h8O6SIoTE2CVdUz_dxr-aiUMflk9_/exec";
 
 const bot = new TelegramBot(token);
 
